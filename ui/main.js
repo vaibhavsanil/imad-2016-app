dfb3b4267d376bca640f69b53a18e14d7e182ce6
vaibhavsanil <vaibhav.sanil007@gmail.com>
2016-10-03 15:02:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,31 @@
-console.log('Loaded!');
+// Counter Code
+
+var button = document.getElementById('counter');
+button.onclick = function(){
+    
+    // get value from counter endpoint
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take Some Action
+          if(request.status==200){
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML=counter.toString();
+    
+          }    
+            
+        }
+        
+  };
+
+     //Make the request
+     
+     request.open('GET','http://vaibhavsanil.imad.hasura-app.io/counter',true);
+     request.send(null);
+    
+    
+};
