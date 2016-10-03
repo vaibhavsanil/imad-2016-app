1f82e667eec14136e5e1ae9d02fee220f4363dc8
vaibhavsanil <vaibhav.sanil007@gmail.com>
2016-10-03 23:30:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ var names = [];
 app.get('/submit-name/:name', function(req,res){
     //Get the name from the request
     
-    var name = req.params.name;
+    var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
