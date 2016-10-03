6ee5fba5a2837e1528037d70ba20eb4916ad5b45
vaibhavsanil <vaibhav.sanil007@gmail.com>
2016-10-03 20:58:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,10 +95,15 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('')
 
 app.get('/:articleName',function(req, res){
                 var articleName= req.params.articleName; 
