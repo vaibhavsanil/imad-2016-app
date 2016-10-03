26de34f1f2b160132214a9ed5cba9afe4787dba6
vaibhavsanil <vaibhav.sanil007@gmail.com>
2016-10-03 21:26:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,10 +105,12 @@ app.get('/ui/madi.png', function (req, res) {
 
 
 var names = [];
+
 app.get('/submit-name/:name', function(res,req){
     //Get the name from the request
     
-    var name = req.params.name ;
+    var name = req.params.name;
+   
     names.push(name);
     
     
