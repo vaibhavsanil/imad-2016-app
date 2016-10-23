f4e93b9260cccffaa79a89f35154bf6e916244b6
vaibhavsanil <vaibhav.sanil007@gmail.com>
2016-10-23 22:16:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
+  //Make a select statement & return the query with the results
+  pool.query('SELECT * FROM test',function(err,result){
+      if(err){
+       res.status(500).send(err.toString());
+      } else {
+           res.send(JSON.stringify(result));  
+      }
+  });
+  //Return a response with results
   
 });
 
