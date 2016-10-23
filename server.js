5fde8762fe3f66e065bb8a6084fd64725951c418
vaibhavsanil <vaibhav.sanil007@gmail.com>
2016-10-23 23:00:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,20 @@ app.get('/counter',function(req,res){
 
 app.get('/articles/:articleName',function(req,res){
               
-             var articleName = req.params.articleName; 
+
+             
+             pool.query("SELECT * FROM article WHERE title =" + req.params.articleName, function(err,result){
+                 if(err){
+                     res.status(500).send(err.toString());
+                 } else {
+                     if(result.rows.length === 0){
+                         res.status(404).send('Article Not Found');
+                     } else {
+                         var articleData = result.rows[0];
+                         res.send(createTemplate(articleData)); 
+                     }
+                 }
+             });
          
         });
 
