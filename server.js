661a3ab45a3adf21f9ad9d92a9be4c71403177c4
vaibhavsanil <vaibhav.sanil007@gmail.com>
2016-10-23 22:29:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/test-db', function (req, res) {
       if(err){
        res.status(500).send(err.toString());
       } else {
-           res.send(JSON.stringify(result));  
+           res.send(JSON.stringify(result.rows));  
       }
   });
   //Return a response with results
