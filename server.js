d87ffb565aed38014d3d191084c206bb72e9731c
vaibhavsanil <vaibhav.sanil007@gmail.com>
2016-10-23 23:55:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/articles/:articleName',function(req,res){
               
 
              
-             pool.query("SELECT * FROM article WHERE title =$1"[req.params.articleName], function(err,result){
+             pool.query("SELECT * FROM article WHERE title =$1",[req.params.articleName], function(err,result){
                  if(err){
                      res.status(500).send(err.toString());
                  } else {
